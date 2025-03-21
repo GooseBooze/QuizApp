3d6f4e2f32414c72b7aa2782607e15c3b351e450
GooseBooze <gard4706@gmail.com>
2025-03-22 00:25:58 +0100

    Master Commit

--- seed.js
+++ seed.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const Quiz = require('./models/Quiz'); // Adjust the path to your Quiz model
 
@@ -9,12 +10,15 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
   .then(() => {
     // Add initial quiz data
     return Quiz.insertMany([
-      { title: 'Sample Quiz 1', questions: [{ question: 'What is 2 + 2?', answers: ['3', '4', '5'], correctAnswers: [1] }] },
-      { title: 'Sample Quiz 2', questions: [{ question: 'What is the capital of France?', answers: ['Berlin', 'Madrid', 'Paris'], correctAnswers: [2] }] },
+      { navn: 'Sample Quiz 1', questions: [{ question: 'What is 2 + 2?', answers: ['3', '4', '5'], correctAnswers: [1] }] },
+      { navn: 'Sample Quiz 2', questions: [{ question: 'What is the capital of France?', answers: ['Berlin', 'Madrid', 'Paris'], correctAnswers: [2] }] },
     ]);
   })
   .then(() => {
     console.log('Database seeded');
     mongoose.connection.close();
   })
-  .catch(err => console.error(err));
+  .catch(err => {
+    console.error('Error during seeding:', err);
+    mongoose.connection.close();
+  });
